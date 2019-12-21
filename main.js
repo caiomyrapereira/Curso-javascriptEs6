2d4a26f7ad2429edd9cf1983d26ad7c53f2efda1
caio myra pereira <caiomyrapereira@hotmail.com>
2019-12-21 17:58:26 -0300

    add Rest/Spread

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert('initialize...')
+// Rest
+
+const person = {
+    name: 'caio',
+    age: 22,
+    sex: 'male'
+}
+
+const { name, ...rest } = person;
+
+console.log(name)
+console.log(rest)
+
+//Spread
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2]
+const person2 = {...person, name: 'daniel' }
+
+console.log(arr3)
+console.log(person2)
